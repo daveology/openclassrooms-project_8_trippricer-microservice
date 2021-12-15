cb940fe00142b0c0408b234babfe758a419344b0
David <d6797125@pm.me>
2021-12-15 11:40:40 +0100

    feat: add getPrice to TripPricerController

--- src/main/java/com/tourguide/trippricer/controller/TripPricerController.java
+++ src/main/java/com/tourguide/trippricer/controller/TripPricerController.java
@@ -1,9 +1,15 @@
 package com.tourguide.trippricer.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import tripPricer.Provider;
+
+import java.util.List;
 
 @RestController
 public class TripPricerController {
 
+    public List<Provider> getPrice() {
 
+
+    }
 }
