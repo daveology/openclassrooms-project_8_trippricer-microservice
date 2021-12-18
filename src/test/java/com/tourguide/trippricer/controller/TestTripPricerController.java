7438f31486b5d5fb4b4ca9a8a56a18b648b9e8fb
David <d6797125@pm.me>
2021-12-18 20:33:09 +0100

    test(controller): add shouldAccessTripDeals endpoint unit test

--- src/test/java/com/tourguide/trippricer/controller/TestTripPricerController.java
+++ src/test/java/com/tourguide/trippricer/controller/TestTripPricerController.java
@@ -6,10 +6,25 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class TestTripPricerController {
 
     @Autowired
     private MockMvc mock;
+
+    @Test
+    public void shouldAccessTripDeals() throws Exception {
+
+        mock.perform(get("/getTripPrice").param("key", "").param("attractionUuid", String.valueOf(UUID.randomUUID())).param("adultsCount", "0").param("childrenCount", "0").param("nightsNumber", "0").param("rewardsPoints", "0"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(5)));
+    }
 }
